cf5a5597bf51d9a856bf561ec84131b7cb6cf269
arkayzdev <franck.zhuang@gmail.com>
2025-02-04 00:46:45 +0100

    refactor(model): Initialize project architecture

---
+++ src/models/class_builder.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass
+class ClassBuilder:
+    @classmethod
+    def build_attributes(
+        cls, attributes_name: List[str]
+    ) -> dict[Optional[str], Optional[object]]:
+        return {attr: None for attr in attributes_name}
+
+    @classmethod
+    def create_class(cls, class_name: str, attributes_name: List[str]) -> object:
+        def print_class(self):
+            print(f"Class Name: {self.__class__.__name__}")
+            print("Attributes:")
+            for attr, value in vars(self).items():
+                print(f"  - {attr}: {value}")
+
+        def __init__(self, *args):
+            if len(args) != len(self.__class__.attributes):
+                raise ValueError(
+                    f"Expected {len(self.__class__.attributes)} arguments,"
+                    f"got {len(args)}"
+                )
+            for attr, value in zip(self.__class__.attributes, args):
+                setattr(self, attr, value)
+
+        new_class = cls.build_attributes(attributes_name=attributes_name)
+
+        new_class["__init__"] = __init__
+        new_class["print_class"] = print_class
+        new_class["attributes"] = attributes_name
+
+        return type(class_name, (object,), new_class)
+
+
+class CsvParser:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def read(self):
+        with open(self.file_path, "r") as file:
+            return file.read()
