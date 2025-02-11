7ce671dae22ac71c7cfcceafde6cf257ba489965
arkayzdev <franck.zhuang@gmail.com>
2025-02-11 01:10:28 +0100

    feat(file): Add json reader

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.models.class_builder import ClassBuilder
-from src.models.csv_utils import CsvUtils
+from src.models.file_utils import CsvUtils, JsonUtils
 
 
 # TEST
@@ -18,10 +18,21 @@ def test_builder():
 
 
 def test_csv_to_class():
-    data = CsvUtils.read_csv(file_path="data/test.csv")
+    data = CsvUtils.read_file(file_path="data/test.csv")
 
     obj_builder = ClassBuilder.create_class(
-        class_name="test", attributes_name=data["attributes_name"]
+        class_name="test", attributes_name=data["attributes"]
+    )
+
+    for d in data["data"]:
+        obj_builder(*d).print_class(oneline=True)
+
+
+def test_json_to_class():
+    data = JsonUtils.read_file(file_path="data/test.json")
+
+    obj_builder = ClassBuilder.create_class(
+        class_name="test", attributes_name=data["attributes"]
     )
 
     for d in data["data"]:
@@ -30,3 +41,4 @@ def test_csv_to_class():
 
 if __name__ == "__main__":
     test_csv_to_class()
+    test_json_to_class()
